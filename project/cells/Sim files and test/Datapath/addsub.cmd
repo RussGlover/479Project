c83cf1e466b303c4650e6e70eabffe6a69cf2017
aruskey <aruskey@shaw.ca>
2015-04-13 23:26:44 -0700

    2 more tests

---
+++ project/cells/Sim files and test/Datapath/addsub.cmd
@@ -0,0 +1,52 @@
+w A B add c z x
+stepsize 50
+
+l a b add c
+s
+
+h a
+l b add c 
+s
+
+h b
+l a add c
+s
+
+h c
+l a b add
+s
+
+h a b
+l c add
+s
+
+h a c
+l b add
+s
+
+h b c 
+l a add
+s
+
+h a b c
+l add
+s
+
+
+//sub
+h add c
+l a b 
+s
+
+l a 
+h b
+s
+
+l b 
+h a
+s
+
+h a b
+s
+
+
