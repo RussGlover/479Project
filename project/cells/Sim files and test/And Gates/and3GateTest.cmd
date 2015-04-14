557a311a19fd24530d3ff6d29c6cd4c485ee9547
Russill Glover <russillg@gmail.com>
2015-04-13 23:17:11 -0700

    More tests

---
+++ project/cells/Sim files and test/And Gates/and3GateTest.cmd
@@ -0,0 +1,16 @@
+vector in A B C
+h Vdd
+l Gnd
+w Out A B C
+set in 000
+s
+set in 001
+s
+set in 010
+s
+set in 100
+s
+set in 111
+s
+exit
+
---
+++ project/cells/Sim files and test/And Gates/andGateTest.cmd
@@ -0,0 +1,13 @@
+vector in A B
+h Vdd
+l Gnd
+w Out A B
+set in 00
+s
+set in 01
+s
+set in 10
+s
+set in 11
+s
+exit
